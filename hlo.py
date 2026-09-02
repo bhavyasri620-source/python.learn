93723ecd0407eef4a86a990fefe8a463081fe763
BHAVYA SRI YARAKARAJU <bhavyasri620@gmail.com>
2026-09-02 21:54:38 +0530

    Add files via upload

---
+++ hlo.py
@@ -0,0 +1,30 @@
+#variables 
+"""hi_50=90 
+print(hi_50)"""
+#comments
+""" this is a comment it will not executed """
+#numbers
+"""a=10 #a is int 
+b=int(10.5)#conversion floate into int
+print(a+b)
+
+power=2**3
+print(power)"""
+#strings
+"""a="bhavya"
+b="sri"
+print(a+b)
+print(len(a))"""
+#booleon (<,>,==,!=,<=,>=)
+"""age=int(input("enter you :"))
+can_vote=age >= 18
+print(can_vote)"""
+#arithmithic 
+"""a=int(input("enter the a value "))
+b=int(input ("enter the b value "))
+print("a+b,a-b,a*b,a/b,a%b,a**b \n",a+b,a-b,a*b,a/b,a%b,a**b)"""
+#logical(and,or,not)
+age=12
+has_licence=True
+can_drive=age>=16 and has_licence
+print(can_drive)
